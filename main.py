36e20d7fd05b1391155d5717df012b953ded5986
Lyebon <sebas25garcia@gmail.com>
2024-12-17 22:04:16 +0000

    The character count

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import string
+
+#Finding the most frequent letter
+#Sorting the results
+#Calculating letter frequencies as percentages
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    words = num_words(text)
+    leters = count_chars(text)
+    print(words, leters)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
+def count_chars(text):
+    lower_words = text.lower()
+    char_count = { }
+    alphabet = string.ascii_lowercase
+    for char in lower_words:
+        if char in alphabet:
+            char_count[char] = char_count.get(char, 0) + 1
+    return char_count
+
+
+def num_words(text):
+    words = text.split()
+    return len (words)
 
 main()
